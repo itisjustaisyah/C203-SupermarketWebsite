8fc3cd7e2ff1f7cf81280c6074bbaf7bcb5cf383
itisjustaisyah <123053209+itisjustaisyah@users.noreply.github.com>
2023-05-31 16:51:04 +0800

    composer files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '8a74c35cde9bf3b5b55dc71f32f47fe8924a993a',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '8a74c35cde9bf3b5b55dc71f32f47fe8924a993a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
